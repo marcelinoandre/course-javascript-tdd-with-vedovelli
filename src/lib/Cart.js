d59567711fde2ed94cd68d6e91ce135a78e5585b
Andre Marcelino <marcelinoandre@gmail.com>
2020-12-10 15:56:58 -0300

    feat:  cart - add tests in discounts aply

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,5 +1,18 @@
 import find from 'lodash/find';
 import remove from 'lodash/remove';
+import Dinero from 'dinero.js';
+
+const Money = Dinero;
+
+Money.defaultCurrency = 'BRL';
+Money.defaultPrecision = 2;
+
+const calculatePercentageDiscount = (amount, item) => {
+  if (item.condition?.percentage && item.quantity > item.condition.minimum) {
+    return amount.percentage(item.condition.percentage);
+  }
+  return Money({ amount: 0 });
+};
 
 export class Cart {
   items = [];
@@ -13,13 +26,32 @@ export class Cart {
 
   getTotal() {
     return this.items.reduce((accumulator, item) => {
-      return accumulator + item.quantity * item.product.price;
-    }, 0);
+      const amount = Money({ amount: item.quantity * item.product.price });
+      const  discount = calculatePercentageDiscount(amount, item)
+
+      return accumulator.add(amount).subtract(discount);
+    }, Money({ amount: 0 }));
   }
 
   remove(product) {
     remove(this.items, { product });
 
     return this.getTotal();
+  }
+
+  summary() {
+    const total = this.getTotal().getAmount();
+    const items = this.items;
+
+    return { total, items };
+  }
+
+  checkout() {
+    const total = this.getTotal();
+    const items = this.items;
+
+    this.items = [];
+
+    return { total, items };
   }
 }
--- src/lib/cart.spec.js
+++ src/lib/cart.spec.js
@@ -14,45 +14,101 @@ describe('Cart', () => {
   beforeEach(() => {
     cart = new Cart();
   });
-  it('should return 0 when getTotal() is executed in a newly created instance', () => {
-    expect(cart.getTotal()).toEqual(0);
+  describe('getTotal()', () => {
+    it('should return 0 when getTotal() is executed in a newly created instance', () => {
+      expect(cart.getTotal().getAmount()).toEqual(0);
+    });
+
+    it('should multiply quantity and price and receive the total amount', () => {
+      const item = {
+        product,
+        quantity: 2 // 70776
+      };
+
+      cart.add(item);
+      expect(cart.getTotal().getAmount()).toEqual(70776);
+    });
+
+    it('should ensure no more than on product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2
+      });
+      cart.add({
+        product,
+        quantity: 1
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(35388);
+    });
+
+    it('should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 2
+      });
+      cart.add({
+        product: product2,
+        quantity: 1
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal().getAmount()).toEqual(41872);
+    });
   });
 
-  it('should multiply quantity and price and receive the total amount', () => {
-    const item = {
-      product,
-      quantity: 2 // 70776
-    };
+  describe('checkout()', () => {
+    it('should return an object with the total and the list of items', () => {
+      cart.add({
+        product,
+        quantity: 5
+      });
 
-    cart.add(item);
-    expect(cart.getTotal()).toEqual(70776);
+      cart.add({
+        product: product2,
+        quantity: 3
+      });
+      expect(cart.summary()).toMatchSnapshot();
+      expect(cart.getTotal().getAmount()).toBeGreaterThan(0);
+    });
+
+    it('should return an object with the total and the list of items', () => {
+      cart.add({
+        product,
+        quantity: 5
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3
+      });
+      expect(cart.checkout()).toMatchSnapshot();
+    });
+
+    it('should reset the cart when checkout() is called', () => {
+      cart.add({
+        product: product2,
+        quantity: 3
+      });
+      expect(cart.getTotal().getAmount()).toEqual(125616);
+    });
   });
 
-  it('should ensure no more than on product exists at a time', () => {
-    cart.add({
-      product,
-      quantity: 2
-    });
-    cart.add({
-      product,
-      quantity: 1
-    });
-
-    expect(cart.getTotal()).toEqual(35388);
-  });
-
-  it('should update total when a product gets included and then removed', () => {
-    cart.add({
-      product,
-      quantity: 2
-    });
-    cart.add({
-      product: product2,
-      quantity: 1
+  describe('special conditions of discounts', () => {
+    it('should apply percentage discount quantity above minimum is passed', () => {
+      const condition = {
+        percentage: 30,
+        minimum: 2
+      };
+      cart.add({
+        product,
+        condition,
+        quantity: 3
+      });
+      expect(cart.getTotal().getAmount()).toEqual(74315);
     });
 
-    cart.remove(product);
-
-    expect(cart.getTotal()).toEqual(41872);
+    it('should apply quantity discount for even quantities', () => {});
   });
 });
